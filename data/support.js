7177c8d9cc8b7004942eced35ec3a03d6b7f1f97
Programmability <programmability@icloud.com>
2019-01-19 16:51:23 +0100

    Add badge list and sponsor list to static JSON data Add sponsor display on event page

--- data/support.js
+++ data/support.js
@@ -10,64 +10,22 @@ var leaderboardTitles    = staticDataJSON["leaderboardTitles"];
 var eventTypes           = staticDataJSON["eventTypes"];
 var eventStages          = staticDataJSON["eventStages"];
 var eventThemes          = staticDataJSON["eventThemes"];
+var eventSponsors        = staticDataJSON["eventSponsors"];
+var badgeList            = staticDataJSON["badgeList"];
 
-
-var badgeList = {
-	
-	casualcouple : { caption : "Casual ARMS Founder", style : "light", criterion: "This is a special badge for the two founders of Casual ARMS" },
-	casualcrew : { caption : "Casual Crew Member", style : "dark", criterion: "Be a current or former member of the Casual Crew" },
-	host : { caption : "Official Lobby Host", style : "dark", criterion: "Volunteer as a host of Casual ARMS lobby events" },
-	cgc : { caption : "Cobra Gloves Club", style : "light", criterion: "Join the CGC and the Cobra hive mind by playing gloves-only Kid Cobra with a CGC member" },
-	
-	clash : { caption : "Casual Clash Participant", style : "light", criterion: "Play in a Casual Clash" },
-	clash_win : { caption : "Casual Clash Champion", style : "hololight", criterion: "Achieve victory in a Casual Clash" },
-	
-	scramble : { caption : "Casual Scramble Participant", style : "dark", criterion: "Play in a Casual Scramble" },
-	scramble_win : { caption : "Casual Scramble Sovereign", style : "holodark", criterion: "Achieve victory in a Casual Scramble" },
-	
-	season_arms     : { caption : "Monthly ARMS Leaderboards", style : "light", criterion: "Finish top 10 in the monthly ARMS leaderboards" },
-	season_arms_win : { caption : "Monthly ARMS Leaderboards Win", style : "hololight", criterion: "Finish as the winner in the monthly ARMS leaderboards" },
-	
-	season_kart     : { caption : "Monthly Mario Kart Leaderboards", style : "light", criterion: "Finish top 10 in the monthly Mario Kart leaderboards" },
-	season_kart_win : { caption : "Monthly Mario Kart Leaderboards Win", style : "hololight", criterion: "Finish as the winner in the monthly Mario Kart leaderboards" },
-	
-	p0 : { caption : "Patreon: Casual Contributor", style : "light", criterion: "Contribute more than $1 on the Patreon for at least one month" },
-	p1 : { caption : "Patreon: Committed Casual",   style : "light", criterion: "Contribute more than $5 on the Patreon for at least one month" },
-	p2 : { caption : "Patreon: True Casual",        style : "dark",  criterion: "Contribute more than $25 on the Patreon for at least one month" },
-	
-	arms_ldb0 : { caption : "ARMS Labs Intern",     style : "light", criterion: "Earn at least 1 coin in ARMS lobbies"},
-	arms_ldb1 : { caption : "ARMS Labs Technician", style : "light", criterion: "Earn at least 100 coins in ARMS lobbies" },
-	arms_ldb2 : { caption : "ARMS Labs Analyst",    style : "light", criterion: "Earn at least 250 coins in ARMS lobbies" },
-	arms_ldb3 : { caption : "ARMS Labs Coder",      style : "light", criterion: "Earn at least 500 coins in ARMS lobbies" },
-	arms_ldb4 : { caption : "ARMS Labs Researcher", style : "light", criterion: "Earn at least 1000 coins in ARMS lobbies" },
-	arms_ldb5 : { caption : "ARMS Labs Scientist",  style : "light", criterion: "Earn at least 2000 coins in ARMS lobbies" },
-	arms_ldb6 : { caption : "ARMS Labs Manager",    style : "light", criterion: "Earn at least 3000 coins in ARMS lobbies" },
-	arms_ldb7 : { caption : "ARMS Labs VP",         style : "light", criterion: "Earn at least 5000 coins in ARMS lobbies" },
-	arms_ldb8 : { caption : "ARMS Labs EVP",        style : "light", criterion: "Earn at least 7000 coins in ARMS lobbies" },
-	arms_ldb9 : { caption : "ARMS Labs Codirector", style : "light", criterion: "Earn at least 10000 coins in ARMS lobbies" },
-	
-	kart_ldb0 : { caption : "Shell Cup Racer",     style : "light", criterion: "Earn at least 1 point in Mario Kart events"},
-	kart_ldb1 : { caption : "Mushroom Cup Racer",  style : "light", criterion: "Earn at least 300 points in Mario Kart events" },
-	kart_ldb2 : { caption : "Banana Cup Racer",    style : "light", criterion: "Earn at least 700 points in Mario Kart events" },
-	kart_ldb3 : { caption : "Flower Cup Racer",    style : "light", criterion: "Earn at least 1500 points in Mario Kart events" },
-	kart_ldb4 : { caption : "Leaf Cup Racer",      style : "light", criterion: "Earn at least 3000 points in Mario Kart events" },
-	kart_ldb5 : { caption : "Star Cup Racer",      style : "light", criterion: "Earn at least 6000 points in Mario Kart events" },
-	kart_ldb6 : { caption : "Egg Cup Racer",       style : "light", criterion: "Earn at least 9000 points in Mario Kart events" },
-	kart_ldb7 : { caption : "Bell Cup Racer",      style : "light", criterion: "Earn at least 15000 points in Mario Kart events" },
-	kart_ldb8 : { caption : "Lightning Cup Racer", style : "light", criterion: "Earn at least 21000 points in Mario Kart events" },
-	kart_ldb9 : { caption : "Special Cup Racer",   style : "light", criterion: "Earn at least 30000 points in Mario Kart events" },
-	
-	splat_ldb0 : { caption : "Grizzco Intern",        style : "light", criterion: "Earn at least 1 point in Splatoon events"},
-	splat_ldb1 : { caption : "Grizzco Trainee",       style : "light", criterion: "Earn at least 300 points in Splatoon events" },
-	splat_ldb2 : { caption : "Grizzco Apprentice",    style : "light", criterion: "Earn at least 700 points in Splatoon events" },
-	splat_ldb3 : { caption : "Grizzco Temp Worker",   style : "light", criterion: "Earn at least 1500 points in Splatoon events" },
-	splat_ldb4 : { caption : "Grizzco Part-Timer",    style : "light", criterion: "Earn at least 3000 points in Splatoon events" },
-	splat_ldb5 : { caption : "Grizzco Full-Timer",    style : "light", criterion: "Earn at least 6000 points in Splatoon events" },
-	splat_ldb6 : { caption : "Grizzco Go-Getter",     style : "light", criterion: "Earn at least 9000 points in Splatoon events" },
-	splat_ldb7 : { caption : "Grizzco Supervisor",    style : "light", criterion: "Earn at least 15000 points in Splatoon events" },
-	splat_ldb8 : { caption : "Grizzco Overachiever",  style : "light", criterion: "Earn at least 21000 points in Splatoon events" },
-	splat_ldb9 : { caption : "Grizzco Profreshional", style : "light", criterion: "Earn at least 30000 points in Splatoon events" },
-};
+for (var game in eventGames)
+{
+	for (var i = 1; i < leaderboardTiers[game].length; ++i)
+	{
+		var tier = leaderboardTiers[game][i];
+		var badge = {
+			"caption"   : leaderboardTitles[game].prefix + tier.name + leaderboardTitles[game].suffix,
+			"style"     : "light",
+			"criterion" : "Earn at least " + tier.start + " points in " + eventGames[game] + " events",
+		};
+		badgeList[game + "_ldb" + (i - 1)] = badge;
+	}
+}
 
 var hostDatabase = [
 	{ "tag" : "CC", "name" : "Rashiko",      "code" : "6822-5055-2423", tier : 2},
